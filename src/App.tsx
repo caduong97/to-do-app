236c975a4a2b67e8e83fce1efbbe8dab29816e4a
Ca Duong <duonghotinhca.97@gmail.com>
2019-12-08 23:40:58 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import Header from './components/Header' 
+import ToDoInput from './components/ToDoInput'
+import ToDoItem from './components/ToDoItem'
+import Card from './components/Card'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className="main">
+        <Card label="add item" status="todo">
+          <ToDoInput/>
+        </Card>
+        <Card label="to-do list" status="doing">
+          <ToDoItem status="doing" content="blahblahblah blah blah blah "/>
+          <ToDoItem status="doing" content="blahblahblah blah blah blah "/>
+          <ToDoItem status="doing" content="blahblahblah blah blah blah "/>
+        </Card>
+        <Card label="completed" status="done">
+          <ToDoItem status="done" content="blahblahblah blah blah blah "/>
+          <ToDoItem status="done" content="blahblahblah blah blah blah "/>
+        </Card>
+      </div>
     </div>
   );
 }
